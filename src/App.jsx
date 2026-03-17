77796dba93feb394ffe92e03f1f592e6bb689932
Irisfundgroup <irisfundgroup@gmail.com>
2026-03-17 02:21:25 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
+import { isRTL, getStoredLang } from '@/lib/i18n'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
