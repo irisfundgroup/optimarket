24fa26a4a92e695644882be431e852dc88c2afe2
Irisfundgroup <irisfundgroup@gmail.com>
2026-03-17 02:22:07 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { isRTL, getStoredLang } from '@/lib/i18n'
